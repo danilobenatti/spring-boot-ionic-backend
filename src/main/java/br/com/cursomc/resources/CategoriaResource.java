40329c680076bf6195861ed4aa52b764a4fb8f94
danilobenatti <danilobenatti@gmail.com>
2021-03-29 08:59:00 -0300

    Criando repository e service para Categoria

--- src/main/java/br/com/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/cursomc/resources/CategoriaResource.java
@@ -1,30 +1,26 @@
 package br.com.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cursomc.domain.Categoria;
+import br.com.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
-	@GetMapping
-	public List<Categoria> listar() {
-		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> lista = new ArrayList<>();
-		
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+	@Autowired
+	private CategoriaService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
+		Categoria categoria = service.buscar(id);
+		return ResponseEntity.ok().body(categoria);
 	}
 
 }
---
+++ src/main/java/br/com/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package br.com.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.cursomc.domain.Categoria;
+import br.com.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repository;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> optional = repository.findById(id);
+		return optional.orElse(null);
+	}
+
+}
